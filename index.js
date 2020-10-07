e2a7cb48705fc48d0fbae8842b8d8deadb7c668b
nvkex <scrbwrm@gmail.com>
2020-10-07 22:34:19 +0530

    logger

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(morgan());
+// Body parser
+app.use(express.json());
+
+app.use(morgan('tiny'));
+
+// // Static serve
+// app.use(express.static('./public'));
 
 app.listen(PORT, () => {
     console.log('Listening on '+PORT);
