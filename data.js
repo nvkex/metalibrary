ac00a6758ccab673fe47dd610343bf2f544c4885
nvkex <scrbwrm@gmail.com>
2020-10-13 18:51:07 +0530

    addition: Moby Dick

--- data.js
+++ data.js
@@ -1,3 +1,8 @@
+/**
+ * This array contains all the data of books in the API
+ * Append new titles to this array without adding any extra keys.
+ * Raise an issue to add an extra key.
+ */
 const books = [
     {
         title: 'The Alchemist',
@@ -8,6 +13,16 @@ const books = [
             'https://en.wikipedia.org/wiki/The_Alchemist_(novel)'
         ],
         summary: 'The Alchemist follows a young Andalusian shepherd in his journey to the pyramids of Egypt, after having a recurring dream of finding a treasure there.'
+    },
+    {
+        title: 'Moby Dick',
+        author: 'Herman Melville',
+        genre: ['Novel', 'Adventure', 'Fiction'],
+        ISBN: '9780679783275',
+        ref: [
+            'https://en.wikipedia.org/wiki/Moby-Dick'
+        ],
+        summary : 'This book is the sailor Ishmael\'s narrative of the obsessive quest of Ahab, captain of the whaling ship Pequod, for revenge on Moby Dick, the giant white sperm whale that on the ship\'s previous voyage bit off Ahab\'s leg at the knee.'
     }
 ];
 
