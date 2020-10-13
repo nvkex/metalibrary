f9dc166d0b860c60e7126734c7e4d314e3bcde57
nvkex <scrbwrm@gmail.com>
2020-10-13 18:39:05 +0530

    books data

---
+++ data.js
@@ -0,0 +1,14 @@
+const books = [
+    {
+        title: 'The Alchemist',
+        author: 'Paulo Coelho',
+        genre:['Quest', 'Adventure', 'Fantasy'],
+        ISBN: '0-06-250217-4',
+        ref: [
+            'https://en.wikipedia.org/wiki/The_Alchemist_(novel)'
+        ],
+        summary: 'The Alchemist follows a young Andalusian shepherd in his journey to the pyramids of Egypt, after having a recurring dream of finding a treasure there.'
+    }
+];
+
+module.exports = { books };
