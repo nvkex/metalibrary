aac66c8d1df43c0bb244210cee0d19e7a384f9a9
nvkex <scrbwrm@gmail.com>
2020-10-23 11:14:57 +0530

    route:get author list

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const { 
     getAll,
-    getRandom
+    getRandom,
+    getAuthors
 } = require('../controllers/index.controller');
 
 router.get('/all', getAll);
 router.get('/random', getRandom);
+router.get('/authors', getAuthors);
 
 module.exports = router;
