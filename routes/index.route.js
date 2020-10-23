aba7ffde7d3805a1456e7cdd400ec72ab8dc3985
nvkex <scrbwrm@gmail.com>
2020-10-23 09:31:56 +0530

    get a random book route

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-const { getAll } = require('../controllers/index.controller');
+const { 
+    getAll,
+    getRandom
+} = require('../controllers/index.controller');
 
 router.get('/all', getAll);
+router.get('/random', getRandom);
 
 module.exports = router;
